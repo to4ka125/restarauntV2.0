ba82258ae3ce6902f079f7e0f46654ff9aa7e121
to4ka1261 <to4ka1261@mail.ru>
2024-12-03 10:02:18 +0300

    Добавили локального пользователя admin Close #1

--- restarauntV2.0/Properties/Settings.Designer.cs
+++ restarauntV2.0/Properties/Settings.Designer.cs
@@ -8,21 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace restarauntV2._0.Properties
-{
+namespace restarauntV2._0.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("admin")]
+        public string LocalAdminLogin {
+            get {
+                return ((string)(this["LocalAdminLogin"]));
+            }
+            set {
+                this["LocalAdminLogin"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("admin")]
+        public string LocalAdminPwd {
+            get {
+                return ((string)(this["LocalAdminPwd"]));
+            }
+            set {
+                this["LocalAdminPwd"] = value;
             }
         }
     }
